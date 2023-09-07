fdbd71f199fc9832d069e8a96990e76f6d7c107c
Marcus Gube <mjosefs@users.noreply.github.com>
2023-09-07 23:33:38 +0200

    version 0.1.5

--- Formula/polytope-cli.rb
+++ Formula/polytope-cli.rb
@@ -1,9 +1,9 @@
 class PolytopeCli < Formula
   desc "CLI for Polytope, the programmable development platform"
   homepage "https://polytope.com"
-  url "https://polytope-cli-releases.s3.eu-north-1.amazonaws.com/polytope-cli-0.1.4-macos-x86-64.tar.gz"
-  sha256 "bb8a11df9b246e3bc63b8cfffe1c83d5ad5c18aa2d5b6ea94a0e362c17d457f7"
-  version "0.1.4"
+  url "https://polytope-cli-releases.s3.eu-north-1.amazonaws.com/polytope-cli-0.1.5-macos-x86-64.tar.gz"
+  sha256 "d7f99bce7bd72a96f390dd39ffa80474afb0d8d60d0afdf8a8f1ec2a20dcc1cf"
+  version "0.1.5"
 
   def install
     libexec.install "polytope"
