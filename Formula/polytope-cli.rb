66fc61dd99c27c4f070a4a94d3d7b8f004553e4a
André Eriksson <aeriksson@users.noreply.github.com>
2024-03-05 13:23:01 +0100

    Release 0.1.15

--- Formula/polytope-cli.rb
+++ Formula/polytope-cli.rb
@@ -1,14 +1,30 @@
 class PolytopeCli < Formula
-  desc "CLI for Polytope, the programmable development platform"
+  desc "The Polytope CLI"
   homepage "https://polytope.com"
-  url "https://polytope-cli-releases.s3.eu-north-1.amazonaws.com/polytope-cli-0.1.15-macos-x86-64.tar.gz"
-  sha256 "8295ec968149ca429484f3c938d7f506f9274bd6824356274df57850ef9298ab"
   version "0.1.15"
 
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://polytope-cli-releases.s3.eu-north-1.amazonaws.com/pt-0.1.15-macos-amd64.xz"
+      sha256 "c509b8cf0c5a56248b079a92e65f5581cfe61977ee9725331e43fc946d2ff308"
+    elsif Hardware::CPU.arm?
+      url "https://polytope-cli-releases.s3.eu-north-1.amazonaws.com/pt-0.1.15-macos-aarch64.xz"
+      sha256 "c509b8cf0c5a56248b079a92e65f5581cfe61977ee9725331e43fc946d2ff308"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://polytope-cli-releases.s3.eu-north-1.amazonaws.com/pt-0.1.15-linux-amd64.xz"
+      sha256 "c509b8cf0c5a56248b079a92e65f5581cfe61977ee9725331e43fc946d2ff308"
+    elsif Hardware::CPU.arm?
+      odie "We don't provide ARM builds on Linux yet – please let us know if you want this!"
+    end
+  end
+
   def install
-    libexec.install "polytope"
-    bin.install_symlink libexec/"polytope" => "polytope-cli"
-    bin.install_symlink libexec/"polytope" => "pt"
+    libexec.install "pt-#{Hardware::CPU.arch}"
+    bin.install_symlink "libexec/pt-#{Hardware::CPU.arch}" => "pt"
   end
 
   test do
