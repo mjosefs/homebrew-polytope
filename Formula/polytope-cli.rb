4ed6a5814442dcc5270b447d11e55addf1096bd4
André Eriksson <aeriksson@users.noreply.github.com>
2024-03-08 14:56:06 +0100

    Release 0.1.18

--- Formula/polytope-cli.rb
+++ Formula/polytope-cli.rb
@@ -1,22 +1,22 @@
 class PolytopeCli < Formula
   desc "The Polytope CLI"
   homepage "https://polytope.com"
-  version "0.1.17"
+  version "0.1.18"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://polytope-cli-releases.s3.eu-north-1.amazonaws.com/pt-0.1.17-macos-amd64.xz"
-      sha256 "8e7c5f6594c03d3f471e85ffc9ab0b24d8a4e36037d160ca38c9aeb63213be2a"
+      url "https://polytope-cli-releases.s3.eu-north-1.amazonaws.com/pt-0.1.18-macos-amd64.xz"
+      sha256 "5347f6442cf177259bf667ccc1260a4cda93172b677b3b9f053587fc6b5a8257"
     elsif Hardware::CPU.arm?
-      url "https://polytope-cli-releases.s3.eu-north-1.amazonaws.com/pt-0.1.17-macos-arm64.xz"
-      sha256 "dd2f4d62aadf95db4f05c348d197ae622ff15b57a15eb44b506b6f6188d27b24"
+      url "https://polytope-cli-releases.s3.eu-north-1.amazonaws.com/pt-0.1.18-macos-arm64.xz"
+      sha256 "2fac79d44964c18ba47f859dfb5d91c4788af928a8a6c52afe006c6f288ea24e"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://polytope-cli-releases.s3.eu-north-1.amazonaws.com/pt-0.1.17-linux-amd64.xz"
-      sha256 "7589b5b4ac03d2381b77f606c7af8be50a14d9ff4dce2eebd2805bc1e8841add"
+      url "https://polytope-cli-releases.s3.eu-north-1.amazonaws.com/pt-0.1.18-linux-amd64.xz"
+      sha256 "5a639718894014cc0faf6f3fd5e0ec77e2924ae71d2b0609aa4569baebcdb71c"
     elsif Hardware::CPU.arm?
       odie "We don't provide ARM builds on Linux yet – please let us know if you want this!"
     end
@@ -25,7 +25,7 @@ class PolytopeCli < Formula
   def install
     os = "#{OS.mac? ? 'macos' : 'linux'}"
     arch = "#{Hardware::CPU.intel? ? 'amd64' : 'arm64'}"
-    filename = "pt-0.1.17-#{os}-#{arch}"
+    filename = "pt-0.1.18-#{os}-#{arch}"
     bin.install filename => "pt"
   end
 
